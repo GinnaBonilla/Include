ab1286a4ee5690e973a4046173cc6d2659caf8c4
GinnaBonilla <GinnaBonilla@users.noreply.github.com>
2016-10-10 12:26:47 -0500

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,17 @@
+<?php 
+
+/*	Código para la conexión a la base de datos,
+	donde "localhost" es el servidor que estamos utilizando,
+	"root" es el usuario del anterior,
+	"", este espacio es por si tiene clave o contraseña,
+	"bd_analiza" y el ultimo campo es para la base de datos
+	que se va a conectar, en este caso se lleva este nombre.
+*/
+	$conexion= mysqli_connect("localhost","root","","bd_analiza");
+
+ ?>
+
+<div>
+	<img src="globo.jpg">
+</div>
+
---
+++ include.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<div>
+		Hola
+	</div>
+
+<?php include ('conexion.php'); ?>
+
+</body>
+</html>
